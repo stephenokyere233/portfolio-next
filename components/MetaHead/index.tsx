36ec6f00c7e3ddc3872e15991c69953d38b61331
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-06-27 21:35:55 +0000

    add: seo

---
+++ components/MetaHead/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Head from "next/head";
+
+const MetaHead = () => {
+  return (
+    <Head>
+      <title>Stephen Okyere — Frontend Developer</title>
+      <meta name="title" content="Stephen Okyere — Frontend Developer" />
+      <meta
+        name="description"
+        content="Skilled frontend developer with expertise in React, Next.js, and Tailwind. I craft stunning, intuitive websites that deliver exceptional user experiences. Turning your vision into reality on the web, one pixel at a time."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://devsteve.pro/" />
+      <meta property="og:title" content="Stephen Okyere — Frontend Developer" />
+      <meta
+        property="og:description"
+        content="Skilled frontend developer with expertise in React, Next.js, and Tailwind. I craft stunning, intuitive websites that deliver exceptional user experiences. Turning your vision into reality on the web, one pixel at a time."
+      />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/devsteveserver/image/upload/v1687901242/Trailers_Home_3_xv3spg.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://devsteve.pro/" />
+      <meta
+        property="twitter:title"
+        content="Stephen Okyere — Frontend Developer"
+      />
+      <meta
+        property="twitter:description"
+        content="Skilled frontend developer with expertise in React, Next.js, and Tailwind. I craft stunning, intuitive websites that deliver exceptional user experiences. Turning your vision into reality on the web, one pixel at a time."
+      />
+      <meta
+        property="twitter:image"
+        content="https://res.cloudinary.com/devsteveserver/image/upload/v1687901242/Trailers_Home_3_xv3spg.png"
+      />
+    </Head>
+  );
+};
+
+export default MetaHead;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import FooterBanner from "../components/Footer/banner";
+import MetaHead from "../components/MetaHead";
 import About from "../components/Sections/About";
 import Hero from "../components/Sections/Hero";
 import Skills from "../components/Sections/Skills";
@@ -6,10 +7,10 @@ import Testimonials from "../components/Sections/Testimonials";
 import WhatIDo from "../components/Sections/WhatIDo";
 import Projects from "../components/Sections/Works";
 
-
 export default function Home() {
   return (
     <>
+      <MetaHead />
       <Hero />
       <WhatIDo />
       <About />
